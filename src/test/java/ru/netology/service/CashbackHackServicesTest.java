27c9bb185122cb4a14d48f1df20b9a369db30b35
Artem Egorov <Artem-Egorov13@mail.ru>
2023-01-16 18:31:31 +0300

    junit-platform

--- src/test/java/ru/netology/service/CashbackHackServicesTest.java
+++ src/test/java/ru/netology/service/CashbackHackServicesTest.java
@@ -3,19 +3,18 @@ package ru.netology.service;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-public class CashbackHackServiceTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+public class CashbackHackServicesTest {
     @Test
-    public void shouldCalculateRemain() {
+    public void shouldCalculateRemainWith999() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 900;
-        int expected = 100;
+        int amount = 999;
+        int expected = 1;
         int actual = cashbackHackService.remain(amount);
         Assert.assertEquals(actual, expected);
     }
-
-    @Test
+    @org.junit.jupiter.api.Test
     public void shouldCalculateRemainWith1() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1;
@@ -23,12 +22,11 @@ public class CashbackHackServiceTest {
         int actual = cashbackHackService.remain(amount);
         Assert.assertEquals(actual, expected);
     }
-
-    @Test
-    public void shouldCalculateRemainWith999() {
+    @org.junit.jupiter.api.Test
+    public void shouldCalculateRemain() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 999;
-        int expected = 1;
+        int amount = 900;
+        int expected = 100;
         int actual = cashbackHackService.remain(amount);
         Assert.assertEquals(actual, expected);
     }
